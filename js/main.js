5d6de9f79cd645226136eae637b2aa6d63eaa9cb
mahmudov-ozodbek <mahmudovozodbek53@gmail.com>
2022-10-23 22:12:46 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let number1 = +prompt("Birinchi raqamni kiriting!");
+let number2 = +prompt("Ikkinchi raqamni kiriting!");
+let amal = +prompt("Amalni kiriting!");
+
+if (amal == "+") {
+  console.log(number1 + number2);
+} else if (amal == "+") {
+  console.log(number1 + number2);
+} else if (amal == "+") {
+  console.log(number1 + number2);
+}
